d132cc2fea3365231358a04b280f732f52eba6d2
Peter <peterzhou43@gmail.com>
2020-11-07 17:51:16 -0800

    update find corner logic

--- Runtime/NavMeshGenerator/PNavMeshFindCornerPass.cs
+++ Runtime/NavMeshGenerator/PNavMeshFindCornerPass.cs
@@ -13,58 +13,24 @@ namespace Parallel.Pathfinding
             {
                 foreach (PNavEdgeLoop edgeLoop in island.edgeLoops)
                 {
-                    PNavNode previousNode = null;
+                    PNavNode previousNode = edgeLoop.nodes[edgeLoop.nodes.Count - 1];
                     Vector2 direction = Vector2.zero;
-                    int currentEdgeCount = 0;
-                    bool skipCorner = false;
+
                     foreach (PNavNode node in edgeLoop.nodes)
                     {
                         node.isCorner = false;
-                        if (currentEdgeCount == 0)
-                        {
-                            previousNode = node;
-                            node.isCorner = true;
-                            skipCorner = true;
-                            node.angle = 0;
-                            currentEdgeCount++;
-                            island.corners.Add(node);
-                        }
-                        else if (currentEdgeCount == 1)
-                        {
-                            direction = new Vector2(node.point.x - previousNode.point.x, node.point.z - previousNode.point.z).normalized;
-                            previousNode = node;
-                            node.angle = 0;
-                            currentEdgeCount++;
-                        }
-                        else
-                        {
-                            Vector2 newDirection = new Vector2(node.point.x - previousNode.point.x, node.point.z - previousNode.point.z);
-                            float angle = Vector2.Angle(newDirection, direction);
 
-                            if (angle >= 45 && !skipCorner)
-                            {
-                                //end current edge
-                                previousNode.angle = angle;
-                                previousNode.isCorner = true;
-                                island.corners.Add(previousNode);
+                        Vector2 newDirection = new Vector2(node.point.x - previousNode.point.x, node.point.z - previousNode.point.z).normalized;
+                        float angle = Vector2.Angle(newDirection, direction);
+                        previousNode.angle = angle;
 
-                                //new edge
-                                direction = newDirection;
-                                previousNode = node;
-                                node.angle = 0;
-                                currentEdgeCount = 2;
-                                skipCorner = true;
-                            }
-                            else
-                            {
-                                //add to current edge
-                                previousNode.angle = angle;
-                                direction = (0.2f * newDirection + direction).normalized;
-                                previousNode = node;
-                                currentEdgeCount++;
-                                skipCorner = false;
-                            }
+                        if (angle >= 45)
+                        {
+                            previousNode.isCorner = true;
                         }
+
+                        direction = newDirection;
+                        previousNode = node;
                     }
                 }
             }
